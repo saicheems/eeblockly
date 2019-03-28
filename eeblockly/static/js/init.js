f2cdbd2b6e6f37a1bffe2f5e352b9a2ae5e7576c
Sai Cheemalapati <saicheems@google.com>
2019-03-28 19:21:37 -0400

    Zoom++.

--- eeblockly/static/js/init.js
+++ eeblockly/static/js/init.js
@@ -18,7 +18,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0},
-    zoom: 1,
+    zoom: 2,
     disableDefaultUI: true
   });
 }
