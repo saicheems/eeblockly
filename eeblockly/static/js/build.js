608fa4d14314562045670d7e62380bf2e7058959
Sai Cheemalapati <saicheems@google.com>
2019-03-28 11:46:32 -0400

    Clean up.

---
+++ eeblockly/static/js/build.js
@@ -0,0 +1,16 @@
+var buildValueNode = function(block) {
+  if (block.type == "Number") {
+    return { "constantValue": block.getFieldValue("value") };
+  } else if (block.type == "String") {
+    return { "constantValue": block.getFieldValue("value") };
+  } else {
+    throw "Unexpected block type: " + block.type;
+  }
+};
+
+var buildExpression = function(block) {
+  return {
+    "values": { "0": buildValueNode(block) },
+    "result": "0"
+  };
+};
